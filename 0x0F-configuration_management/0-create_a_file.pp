4331599397c7a44964d495d6bf08c8ad94df615b
Stuart <sgkur04@gmail.com>
2017-08-18 23:29:43 +0000

    0-create_a_file.pp :change double quotes to single for content

--- 0x0F-configuration_management/0-create_a_file.pp
+++ 0x0F-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # creates a file in /temp
 file { '/tmp/holberton':
-  ensure => file,
-  content => "I love Puppet",
+  ensure  => file,
+  content => 'I love Puppet',
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
