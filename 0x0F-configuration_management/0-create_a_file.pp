8255bddda1b40a5bc31572f103a6636348e7bdd5
Stuart <sgkur04@gmail.com>
2017-08-18 22:36:21 +0000

    0-create_a_file.pp :add description

--- 0x0F-configuration_management/0-create_a_file.pp
+++ 0x0F-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
+# creates a file in /temp
 file { '/tmp/holberton':
   ensure => file,
-  content => "I love Puppet,
+  content => "I love Puppet",
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
