f68dbccb546719ac3893e6b7fb8740ec60dd8356
Stuart Kuredjian <108@holbertonschool.com>
2017-09-10 22:37:42 +0000

    0-strace_is_your_friend.pp : us4 sed instead of file_line in manifest

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,12 @@
-# fix typo extension .phpp typo on line 137 by removeing extra 'p'
-file_line { 'fix: remove trailing \'p\'in extension \'.phpp.\' );':
-  path  => '/var/www/html/wp-settings.php',
-  line  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
-  match => 'require_once\( ABSPATH . WPINC . \'/class-wp-locale.phpp\' \);'
+# turn on display_errors in /etc/php5/apach2/php.ini
+exec { 'fix typo,  extra p in php extension':
+  command => 'sed  -i "s@.phpp@.php@" /var/www/html/wp-settings.php',
+  path    => ['/bin','/sbin','usr/bin','usr/sbin']
 }
+
+# to use this, first run 'puppet module install puppetlabs-stdlib --version 4.19.0'
+# file_line { 'fix typo,  extra p in php extension':
+#  path  => '/var/www/html/wp-settings.php',
+#  line  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
+#  match => 'require_once\( ABSPATH . WPINC . \'/class-wp-locale.phpp\' \);'
+#}
