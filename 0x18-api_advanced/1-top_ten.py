c099af0a80762eb0c8796e963881dd3aa38020f6
Stuart Kuredjian <108@holbertonschool.com>
2017-09-21 21:15:53 +0000

    1-top_ten.py :initial commit

---
+++ 0x18-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+module: 1-top_ten
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    queries the Reddit API (https://www.reddit.com/dev/api/) and
+    prints the titles of the first 10 hot posts listed for a given subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    r = requests.get(url, headers={'User-Agent': 'some use (me)'}).json()
+    data = r.get('data')
+    if data is None:
+        print(None)
+        return
+
+    for elem in r.get('data').get('children'):
+        print(elem.get('data').get('title'))
