5d4c7d31acc398e49a01e5401f0a73169f2ca3f0
Stuart Kuredjian <sgkur04@gmail.com>
2018-10-14 01:39:19 +0000

    test

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,76 +1,65 @@
 #!/usr/bin/python3
-"""100-count"""
-from collections import namedtuple
-import json
+"""
+queries to https://www.reddit.com/dev/api/
+"""
 import requests
 
 
-def count_words(subreddit, word_list, after=None):
-    """  queries the Reddit API for hottest posts in subreddit, 'subreddit' and
-    searches for number of time each word in 'word_list appears'"""
-
-    subreddit = subreddit.lower()
-    if type(word_list) is list:
-        results_dict = {word.lower(): 0 for word in word_list}
+def make_get_request(subreddit, after):
+    """
+    makes reddit get request to hot topics of subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    if after:
+        payload = {'after': after, 'limit': '100'}
     else:
-        results_dict = word_list
-    response = get_articles(subreddit, after=after)
-    if response.status_code != 200:
-        return
-    content = str(response.content, encoding='utf8')
-    obj = get_content_object_form(content)
-    data = obj.data
-    articles = data.children
-    after = data.after
-
-    for article in articles:
-        title = article.data.title.lower()
-        results_dict = update_word_count(title, results_dict)
-
-    if after is None:
-        print_results(results_dict)
-    else:
-        count_words(subreddit, results_dict, after)
-
-
-def update_word_count(title, results_dict):
-    """ counts occurances of 'results_list` keys in `title` and appends them
-    to running results_dict """
-    title = [x for x in title.split()]
-    for word in results_dict:
-        wc = title.count(word)
-        if wc > 0:
-            results_dict[word] += wc
-    return results_dict
-
-
-def get_articles(subreddit, after):
-    """ makes reddit API request to retrive articles """
-    params = {'limit': '100', 'after': after if after else ''}
-    headers = {'User-Agent': 'not-you'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(url=url,
-                            headers=headers,
-                            allow_redirects=False,
-                            params=params)
-
+        payload = {'limit': '100'}
+    header = {'user-agent': 'one-dope-boy', 'over18': 'yes'}
+    response = requests.get(
+        url, headers=header, params=payload, allow_redirects=False
+    )
     return response
 
 
-def print_results(results):
-    """ prints the query results """
+def search_for_words(children, word_list):
+    """
+    searches for words in response
+    """
+    for child in children:
+        title = child.get('data').get('title').lower()
+        title_words = [word for word in title.split()]
+        for word in word_list:
+            count = title_words.count(word)
+            if count > 0:
+                word_list[word] += count
+    return word_list
 
-    results_info = []
-    keys = results.keys()
-    for (i, elem) in enumerate(keys):
-        results_info.append({'key': elem, 'count': results[elem]})
-    results_info = sorted(results_info, key=lambda k: k['count'])[::-1]
-    for elem in results_info:
-        if results[elem['key']] > 0:
-            print("{}: {}".format(elem['key'], elem['count']))
+
+def print_results(word_list):
+    """
+    prints result list
+    """
+    word_list = [
+        [word, count] for word, count in word_list.items() if count > 0
+    ]
+    word_list = sorted(word_list, key=lambda x: x[1], reverse=True)
+    for word in word_list:
+        print('{}: {}'.format(word[0], word[1]))
 
 
-def get_content_object_form(content):
-    """ puts json response into object form """
-    return json.loads(content, object_hook=lambda d: namedtuple(
-        'X', d.keys())(*d.values()))
+def count_words(subreddit, word_list, after=None):
+    """
+    prints the count of top ten hot posts for subreddit
+    """
+    if type(word_list).__name__ == 'list':
+        word_list = {word.lower(): 0 for word in word_list}
+    response = make_get_request(subreddit, after)
+    if response.status_code == 200:
+        data = response.json().get('data')
+        after = data.get('after')
+        children = data.get('children')
+        word_list = search_for_words(children, word_list)
+        if after:
+            count_words(subreddit, word_list, after)
+        else:
+            print_results(word_list)
